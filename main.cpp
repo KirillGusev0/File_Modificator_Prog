a6112d219078297c699de7d60becadcdf6a9d959
KirillGusev0 <43314984+KirillGusev0@users.noreply.github.com>
2024-08-06 11:30:21 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,134 @@
+#include <QCoreApplication>
+#include <QDir>
+#include <QFile>
+#include <QTimer>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+#include <QDebug>
+#include <iostream>
+#include <fstream>
+
+// Глобальные переменные для хранения настроек
+QString inputMask;
+QString outputPath;
+bool deleteInputFiles;
+bool overwriteFiles;
+int pollInterval;
+quint64 xorValue;
+bool singleRun; // Переменная для хранения флага разового запуска
+
+// Функция для обработки файлов
+void processFiles() {
+    QDir directory;
+    // Поиск файлов, соответствующих маске
+    QStringList files = directory.entryList(QStringList() << inputMask, QDir::Files);
+
+    for (const QString &fileName : files) {
+        QFile inputFile(fileName);
+        
+        // Проверка, можно ли открыть файл для чтения
+        if (!inputFile.open(QIODevice::ReadOnly)) {
+            qDebug() << "Не удалось открыть файл для чтения:" << fileName;
+            continue;
+        }
+
+        // Чтение содержимого файла
+        QByteArray fileData = inputFile.readAll();
+        inputFile.close();
+
+        // Выполнение XOR операции
+        for (int i = 0; i < fileData.size(); ++i) {
+            fileData[i] = fileData[i] ^ ((char *)&xorValue)[i % 8];
+        }
+
+        // Формирование имени выходного файла
+        QString outputFileName = outputPath + "/" + fileName;
+
+        // Проверка, требуется ли перезаписывать выходной файл
+        if (QFile::exists(outputFileName) && !overwriteFiles) {
+            int counter = 1;
+            while (QFile::exists(outputFileName)) {
+                outputFileName = outputPath + "/" + fileName + "." + QString::number(counter);
+                counter++;
+            }
+        }
+
+        // Открытие выходного файла для записи
+        QFile outputFile(outputFileName);
+        if (!outputFile.open(QIODevice::WriteOnly)) {
+            qDebug() << "Не удалось открыть файл для записи:" << outputFileName;
+            continue;
+        }
+
+        // Запись измененного содержимого в выходной файл
+        outputFile.write(fileData);
+        outputFile.close();
+
+        // Удаление входного файла, если необходимо
+        if (deleteInputFiles) {
+            inputFile.remove();
+        }
+    }
+
+    // Если разовый запуск, то завершаем приложение после обработки файлов
+    if (singleRun) {
+        QCoreApplication::quit();
+    }
+}
+
+int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
+
+    // Настройка парсера командной строки
+    QCommandLineParser parser;
+    parser.setApplicationDescription("File Modifier");
+    parser.addHelpOption();
+
+    // Определение опций командной строки
+    QCommandLineOption inputMaskOption(QStringList() << "m" << "mask", "Маска входных файлов", "mask");
+    parser.addOption(inputMaskOption);
+
+    QCommandLineOption outputPathOption(QStringList() << "o" << "output", "Путь для сохранения результирующих файлов", "output");
+    parser.addOption(outputPathOption);
+
+    QCommandLineOption deleteOption(QStringList() << "d" << "delete", "Удалять входные файлы");
+    parser.addOption(deleteOption);
+
+    QCommandLineOption overwriteOption(QStringList() << "w" << "overwrite", "Перезаписывать выходные файлы");
+    parser.addOption(overwriteOption);
+
+    QCommandLineOption intervalOption(QStringList() << "i" << "interval", "Периодичность опроса наличия входного файла в мс", "interval", "1000");
+    parser.addOption(intervalOption);
+
+    QCommandLineOption xorValueOption(QStringList() << "x" << "xor", "8-байтовое значение для XOR операции в шестнадцатеричном формате", "xor", "0000000000000000");
+    parser.addOption(xorValueOption);
+
+    // Опция командной строки для разового запуска
+    QCommandLineOption singleRunOption(QStringList() << "s" << "single", "Разовый запуск");
+    parser.addOption(singleRunOption);
+
+    // Парсинг аргументов командной строки
+    parser.process(app);
+
+    // Получение значений из аргументов командной строки
+    inputMask = parser.value(inputMaskOption);
+    outputPath = parser.value(outputPathOption);
+    deleteInputFiles = parser.isSet(deleteOption);
+    overwriteFiles = parser.isSet(overwriteOption);
+    pollInterval = parser.value(intervalOption).toInt();
+    xorValue = parser.value(xorValueOption).toULongLong(nullptr, 16);
+    singleRun = parser.isSet(singleRunOption); // Получение значения для разового запуска
+
+    // Если разовый запуск, выполняем обработку файлов один раз
+    if (singleRun) {
+        processFiles();
+        return 0;
+    } else {
+        // Настройка таймера для периодической проверки наличия файлов
+        QTimer timer;
+        QObject::connect(&timer, &QTimer::timeout, processFiles);
+        timer.start(pollInterval);
+
+        return app.exec();
+    }
+}
